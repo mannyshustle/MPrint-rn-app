f95640605d87f7b40e3088f836580ac15a3b0901
manny <bobokeke0@gmail.com>
2024-03-13 05:45:06 +0000

    chore: Rename from Manny's Designs to MannysDesigns

--- ios/MannyDesignsTests/MannyDesignsTests.m
+++ ios/MannyDesignsTests/MannyDesignsTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface MannysDesignsTests : XCTestCase
+@interface MannyDesignsTests : XCTestCase
 
 @end
 
-@implementation MannysDesignsTests
+@implementation MannyDesignsTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
